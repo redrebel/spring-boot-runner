dbcd91ea3dbfbe19dcd11899b8ca6195251fae5e
Lee, Jung Geun <cjred77@gmail.com>
2018-12-29 12:42:49 +0900

    readme 추가

---
+++ README.md
@@ -0,0 +1,5 @@
+@Commponent로 등록하여 입력파라미터를 받는 스프링부트 프로젝트를 만들 수 있다.
+ApplicationRunner
+CommandLineRunner
+
+만일 순서를 지정하고 싶다면 @Order 를 사용하면 된다.
--- src/main/java/com/springboot/runner/DemoApplicationRunner.java
+++ src/main/java/com/springboot/runner/DemoApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.springboot.runner;
 
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +9,7 @@ import java.util.Arrays;
 
 @Order(2)
 @Component
-public class DemoApplicationRunner implements org.springframework.boot.ApplicationRunner {
+public class DemoApplicationRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("ApplicationRunner Args: " + Arrays.toString(args.getSourceArgs()));
